ff4805d7580d48453edb1fe02f38499e155b3202
loundor <loundor@gmail.com>
2022-07-10 12:14:14 +0200

    Start of project

---
+++ includes/minishell.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stissera <stissera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/10 12:09:04 by stissera          #+#    #+#             */
+/*   Updated: 2022/07/10 12:13:35 by stissera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/signal.h>
+
+#endif
