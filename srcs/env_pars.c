4e28938e859da62eddf38994a6ac1e732924cd00
loundor <loundor@gmail.com>
2022-07-14 13:32:55 +0200

    Start fr_exit, error and free

--- srcs/env_pars.c
+++ srcs/env_pars.c
@@ -6,7 +6,7 @@
 /*   By: stissera <stissera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 14:26:15 by stissera          #+#    #+#             */
-/*   Updated: 2022/07/14 13:26:38 by stissera         ###   ########.fr       */
+/*   Updated: 2022/07/14 13:32:27 by stissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ t_env	*do_env(char **env)
 	{
 		tenv = malloc(sizeof(t_env));
 		if (!tenv)
-			exit(ft_exit(MALLOCERR, 0));
+			exit(ft_exit(MALLOCERR, 0, NULL));
 		tenv->env_var = ft_split(*env, '=');
 		ret = tenv;
 	}
@@ -47,7 +47,7 @@ t_env	*do_env(char **env)
 		parsse = 0;
 		parsse = malloc(sizeof(t_env));
 		if (!parsse)
-			exit(ft_exit(MALLOCERR, 1));
+			exit(ft_exit(MALLOCERR, 1, tenv));
 		parsse->prev_env = tenv;
 		parsse->env_var = ft_split(*env, '=');
 		tenv->next_env = parsse;
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: stissera <stissera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 12:07:29 by stissera          #+#    #+#             */
-/*   Updated: 2022/07/14 13:30:24 by stissera         ###   ########.fr       */
+/*   Updated: 2022/07/14 13:31:38 by stissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main(int argc, char **argv, char **env)
 
 	shell.env = do_env(env);
 	if (!shell.env)
-		exit(ft_exit(ENV, 0));
+		exit(ft_exit(ENV, 0, NULL));
 	if (argc == 1)
 		(void)argv;
 	// Not already implemented..
@@ -30,7 +30,7 @@ int	main(int argc, char **argv, char **env)
 	} */
 
 	if (welcome() != 0)
-		exit (ft_exit(WELCOME_ERR, 1)); // A verifier 1 en 2 ???!!
+		exit (ft_exit(WELCOME_ERR, 1, NULL)); // A verifier 1 en 2 ???!!
 
 	if (!prompt())
 		return (0);
