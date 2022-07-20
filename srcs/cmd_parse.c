eac17fa47ca4e3f73afa1b0a72876efc49a7830d
Sébastien Tisserand <loundor@gmail.com>
2022-07-20 16:32:15 +0200

    Update cmd_parse.c

--- srcs/cmd_parse.c
+++ srcs/cmd_parse.c
@@ -6,7 +6,7 @@
 /*   By: stissera <stissera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 17:10:41 by stissera          #+#    #+#             */
-/*   Updated: 2022/07/20 10:42:49 by stissera         ###   ########.fr       */
+/*   Updated: 2022/07/20 15:07:48 by stissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,56 @@
 static char	*take_path(char *line, t_cmd *cmd)
 {
 	char	*ret;
-	
+	size_t	i;
+	int		slash;
 
-	line = ft_skipspace(line);
+	i = 0;
+	slash = 0;
+	while (line[i] != '\0' && !ft_isspace(line[i]))
+	{
+		if (line[i] == '/')
+			slash++;
+		i++;
+	}
+	if (slash > 0)
+	{
+		while (line[i] != '/')
+			i--;
+	}
+	ret = (char *)malloc(sizeof(char) * (i + 1));
+	if (!ret)
+		ft_exit(MALLOCERR, 0, 0);
+	slash = 0;
+	while (slash != i)
+		ret[slash++] = *line++;
+	ret[slash] = '\0';
 	return (ret);
 }
 
 static char	*take_exec(char *line, t_cmd *cmd)
 {
+	char	*ret;
+	size_t	i[2];
 
+	i[0] = 0;
+	while (!ft_isspace(line[i[0]]) && line[i[0]] != '\0')
+		i[0]++;
+	ret = (char *)malloc(sizeof(char) * (i[0] + 1));
+	if (!ret)
+		ft_exit(MALLOCERR, 0, 0);
+	while (i[1] != i[0])
+		ret[i[1]++] = *line++;
+	ret[i[1]] = '\0';
+	line = ft_skipspace(line);
+	return (ret);
+}
+
+static char	*take_params(char *line, t_cmd *cmd)
+{
+	char	*ret;
+	size_t	i[2];
+
+	return (ret);
 }
 
 t_cmd	*cmd_parse(char *shell, t_cmd *cmd)
@@ -41,10 +82,15 @@ t_cmd	*cmd_parse(char *shell, t_cmd *cmd)
 	if (!line || !new)
 		ft_exit(MALLOCERR, 0, 0);
 	new->path = take_path(&line, new);
+	line = ft_skipspace(line);
 	new->command = take_exec(&line, new);
+	line = ft_skipspace(line);
 	new->param = take_params(&line, new);
+	line = ft_skipspace(line);
 	new->option = take_option(&line, new);
+	line = ft_skipspace(line);
 	new->type = take_operator(&line, new);
+	line = ft_skipspace(line);
 	if (*line != '\0')
 		cmd->next = cmd_parse(line, new);
 	return (cmd);
