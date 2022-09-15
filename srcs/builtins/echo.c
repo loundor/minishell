d587978b7531d7522d021cc5363bff4589e8de75
Sebastien Tisserand <stissera@k2r2p10.42mulhouse.fr>
2022-09-15 14:39:36 +0200

    Fix echo with no newline

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: stissera <stissera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 17:04:04 by alfux             #+#    #+#             */
-/*   Updated: 2022/09/11 10:51:15 by stissera         ###   ########.fr       */
+/*   Updated: 2022/09/15 14:39:08 by stissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ static void	ft_shwarg(char **av, char opt)
 	i = opt;
 	while (*(av + i))
 	{
-		printf("%s", *(av + i++));
+		ft_putstr_fd(*(av + i++), 1);
 		if (*(av + i) && **(av + i - 1))
-			printf(" ");
+			ft_putchar_fd(' ', 1);
 		else if (!*(av + i) && !opt)
-			printf("\n");
+			ft_putchar_fd('\n', 1);
 	}
 }
 
@@ -34,7 +34,7 @@ int	ft_echo(char **av)
 	if (!*(av + 1))
 		return (0 * printf("\n"));
 	av++;
-	if (!ft_strncmp(*av, "-n", 3))
+	if (!ft_strncmp(*av, "-n\0", 3))
 		ft_shwarg(av, 1);
 	else
 		ft_shwarg(av, 0);
